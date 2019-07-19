cb05af27aef4759b3a78d6a5abca77d472798402
Santhose Subramaniam <santhose.subramaniam@gmail.com>
2019-07-18 21:25:15 -0400

    additional format printing

--- src/001-helo-world/hello.go
+++ src/001-helo-world/hello.go
@@ -2,9 +2,11 @@ package main
 
 import "fmt"
 
-var z = 43  // use var to declare outside of function body
-var k int // declare there is a variable with identifier k of type int
-// nil for pointers, false for booleans, 0.0 for floats and 0 for ints.
+var z = 43 // use var to declare outside of function body
+var k int  // declare there is a variable with identifier k of type int
+// nil for pointers, false for booleans, 0.0 for floats and 0 for ints., "" for strings.
+var variable = `this is a "test to include double quotes"` // back quotes not single quote
+
 func main() {
 	foo()
 
@@ -20,13 +22,15 @@ func main() {
 	x := 42
 	fmt.Println(x)
 
+	fmt.Println(variable)
+
+	fmt.Printf("variable is of type: %T\n", variable) //%T prints type of the variable
+
 	x = 43
 
 	y := x * x
 
-	fmt.Println("x= ", x , "y = ", y)
-
-
+	fmt.Println("x= ", x, "y = ", y)
 
 	fmt.Println(z)
 }
